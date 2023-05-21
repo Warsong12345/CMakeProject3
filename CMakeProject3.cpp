97713dbb6b8a16146f2f7c036df4d48c8bb9a688
37631 <37631@K1LLER>
2023-05-21 09:44:11 +0800

    Add project files.

---
+++ CMakeProject3.cpp
@@ -0,0 +1,24 @@
+﻿// CMakeProject3.cpp : Defines the entry point for the application.
+
+
+#include "CMakeProject3.h"
+
+using namespace std;
+class myBase {
+public:	const int int1 = 0xfeel;
+	  int int2 = 0xfee;
+
+	myBase() {
+		std::cout << "int1 is:" << int1 << endl;
+		std::cout << "int2 is:" << int2 << endl;
+		std::cout << sizeof(myBase) << endl;
+	}
+};
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	myBase mybase1;
+	return 0;
+}
+
